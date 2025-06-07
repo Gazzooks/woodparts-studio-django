01f9c5b8f5087c23449fb2274711232903b0191e
Gazzooks <bebonkin@gmail.com>
2025-06-07 19:37:33 -0230

    Added 90 percent of the references un the References app

--- references/urls.py
+++ references/urls.py
@@ -1,11 +1,33 @@
 from django.urls import path
 from . import views
 
-app_name = 'references'  # This registers the namespace
+app_name = 'references'
 
 urlpatterns = [
-    path('wood-types/', views.WoodTypesView.as_view(), name='wood_types'),
-    path('screw-chart/', views.ScrewChartView.as_view(), name='screw_chart'),
-    path('nominal-actual/', views.NominalActualView.as_view(), name='nominal_actual'),
-    path('joint-types/', views.JointTypesView.as_view(), name='joint_types'),
+    # Wood & Materials
+    path('wood-types/', views.wood_types, name='wood_types'),
+    path('nominal-actual-sizes/', views.nominal_actual_sizes, name='nominal_actual_sizes'),
+    path('lumber-grades/', views.lumber_grades, name='lumber_grades'),
+    path('material-properties/', views.material_properties, name='material_properties'),
+
+    # Hardware & Fasteners
+    path('screw-size-chart/', views.screw_size_chart, name='screw_size_chart'),
+    path('bolt-nut-chart/', views.bolt_nut_chart, name='bolt_nut_chart'),
+    path('nail-chart/', views.nail_chart, name='nail_chart'),
+    path('hardware-catalog/', views.hardware_catalog, name='hardware_catalog'),
+
+    # Joinery & Techniques
+    path('joint-types/', views.joint_types, name='joint_types'),
+    path('cutting-techniques/', views.cutting_techniques, name='cutting_techniques'),
+    path('assembly-methods/', views.assembly_methods, name='assembly_methods'),
+
+    # Finishing
+    path('finishing-guide/', views.finishing_guide, name='finishing_guide'),
+    path('stain-colors/', views.stain_colors, name='stain_colors'),
+    path('finish-compatibility/', views.finish_compatibility, name='finish_compatibility'),
+
+    # Standalone references
+    path('documentation/', views.documentation, name='documentation'),
+    path('quick-reference-cards/', views.quick_reference_cards, name='quick_reference_cards'),
+    path('safety-guidelines/', views.safety_guidelines, name='safety_guidelines'),
 ]
--- references/views.py
+++ references/views.py
@@ -1,13 +1,52 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render
 
-class WoodTypesView(TemplateView):
-    template_name = 'references/wood_types.html'
+def wood_types(request):
+    return render(request, 'references/wood_types.html')
 
-class ScrewChartView(TemplateView):
-    template_name = 'references/screw_chart.html'
+def nominal_actual_sizes(request):
+    return render(request, 'references/nominal_actual_sizes.html')
 
-class NominalActualView(TemplateView):
-    template_name = 'references/nominal_actual.html'
+def lumber_grades(request):
+    return render(request, 'references/lumber_grades.html')
 
-class JointTypesView(TemplateView):
-    template_name = 'references/joint_types.html'
+def material_properties(request):
+    return render(request, 'references/material_properties.html')
+
+def screw_size_chart(request):
+    return render(request, 'references/screw_size_chart.html')
+
+def bolt_nut_chart(request):
+    return render(request, 'references/bolt_nut_chart.html')
+
+def nail_chart(request):
+    return render(request, 'references/nail_chart.html')
+
+def hardware_catalog(request):
+    return render(request, 'references/hardware_catalog.html')
+
+def joint_types(request):
+    return render(request, 'references/joint_types.html')
+
+def cutting_techniques(request):
+    return render(request, 'references/cutting_techniques.html')
+
+def assembly_methods(request):
+    return render(request, 'references/assembly_methods.html')
+
+def finishing_guide(request):
+    return render(request, 'references/finishing_guide.html')
+
+def stain_colors(request):
+    return render(request, 'references/stain_colors.html')
+
+def finish_compatibility(request):
+    return render(request, 'references/finish_compatibility.html')
+
+def documentation(request):
+    return render(request, 'references/documentation.html')
+
+def quick_reference_cards(request):
+    return render(request, 'references/quick_reference_cards.html')
+
+def safety_guidelines(request):
+    return render(request, 'references/safety_guidelines.html')
